f45d8b335d54b7ae36f035d473d8fe36552a6cbc
Gustavo Caso <gustavocaso@gmail.com>
2018-03-25 19:17:55 +0200

    make layout use slim

--- spec/web/views/application_layout_spec.rb
+++ spec/web/views/application_layout_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe Web::Views::ApplicationLayout do
   let(:layout)   { Web::Views::ApplicationLayout.new(template, {}) }
   let(:rendered) { layout.render }
-  let(:template) { Hanami::View::Template.new('apps/web/templates/application.html.erb') }
+  let(:template) { Hanami::View::Template.new('apps/web/templates/application.html.slim') }
 
   it 'contains application name' do
     expect(rendered).to include('Web')
