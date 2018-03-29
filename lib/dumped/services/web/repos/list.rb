94a1c31a4c46aa95ff90ccbaca1e293a68062dc5
Gustavo Caso <gustavocaso@gmail.com>
2018-03-29 08:34:45 +0200

    Move specs and service to right folder

--- lib/dumped/services/web/repos/list.rb
+++ lib/dumped/services/web/repos/list.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
 module Services
-  module Repos
-    class List
-      attr_reader :repository
+  module Web
+    module Repos
+      class List
+        attr_reader :repository
 
-      def initialize(repository: RepoRepository.new)
-        @repository = repository
-      end
+        def initialize(repository: RepoRepository.new)
+          @repository = repository
+        end
 
-      def call(language)
-        if language
-          return repository.all if language == Repo::ALL.downcase
-          repository.by_language(language)
-        else
-          repository.all
+        def call(language)
+          if language
+            return repository.all if language == Repo::ALL.downcase
+            repository.by_language(language)
+          else
+            repository.all
+          end
         end
       end
     end
