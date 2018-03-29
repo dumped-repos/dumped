7100753ca031742fdeb47bc9310fedbfe8f28825
Gustavo Caso <gustavocaso@gmail.com>
2018-03-29 08:35:58 +0200

    Use DI for workers

--- lib/workers/auth/import_repos.rb
+++ lib/workers/auth/import_repos.rb
@@ -5,55 +5,14 @@ module Workers
     class ImportRepos
       include Sidekiq::Worker
 
+      attr_reader :user_repos_import
+
+      def initialize(user_repos_import: Services::Users::Repos::Import.new )
+        @user_repos_import = user_repos_import
+      end
+
       def perform(login)
-        response = Http.get(user_repo_url(login))
-        body_parsed = response.parse
-        body_parsed.each do |repo|
-          repo_info = extract_repo_info(repo)
-          create_repo(repo_info, user(login))
-        end
-      end
-
-      private
-
-      def user(login)
-        @user ||= user_repository.by_login(login)
-      end
-
-      def extract_repo_info(repo_hash)
-        {
-          repo_id: repo_hash['id'],
-          owner_login: repo_hash['owner']['login'],
-          owner_avatar_url: repo_hash['owner']['owner_avatar_url'],
-          name: repo_hash['name'],
-          full_name: repo_hash['full_name'],
-          html_url: repo_hash['html_url'],
-          description: repo_hash['description'],
-          repo_created_at: repo_hash['created_at'],
-          homepage: repo_hash['homepage'],
-          size: repo_hash['size'],
-          stargazers_count: repo_hash['stargazers_count'],
-          watchers_count: repo_hash['watchers_count'],
-          language: repo_hash['language'],
-          forks_count: repo_hash['forks_count'],
-          open_issues_count: repo_hash['open_issues_count'],
-        }
-      end
-
-      def create_repo(repo, user)
-        repo_repository.create(**repo, user_id: user.id)
-      end
-
-      def user_repo_url(login)
-        "https://api.github.com/users/#{login}/repos"
-      end
-
-      def user_repository
-        @user_repository ||= UserRepository.new
-      end
-
-      def repo_repository
-        @repo_repository ||= RepoRepository.new
+        user_repos_import.call(login)
       end
     end
   end
--- spec/workers/auth/import_repos_spec.rb
+++ spec/workers/auth/import_repos_spec.rb
@@ -1,30 +1,11 @@
 RSpec.describe Workers::Auth::ImportRepos do
-  let(:user_repo) { UserRepository.new }
-  let(:repo_repository) { RepoRepository.new }
+  let(:user_repos_import) { double('UserReposImport') }
+  let(:login) { 'yoyo' }
 
-  let(:worker) { described_class.new }
+  let(:worker) { described_class.new(user_repos_import: user_repos_import) }
 
-  after do
-    user_repo.clear
-    repo_repository.clear
-  end
-
-  context 'import all the user repos' do
-    let(:user) { user_repo.create(login: 'yoyo') }
-
-    before do
-      stub_request(:get, "https://api.github.com/users/yoyo/repos").
-         with(  headers: {
-       	  'Connection'=>'close',
-       	  'Host'=>'api.github.com',
-       	  'User-Agent'=>'http.rb/3.0.0'
-           }).
-         to_return(status: 200, body: http_fixture('github_repos'), headers: {'Content-Type'  => 'application/json; charset=utf-8'})
-    end
-
-    it 'creates all repos associated with the user' do
-      worker.perform(user.login)
-      expect(repo_repository.all.to_a.count).to eq 2
-    end
+  it 'calls user_repos_import' do
+    expect(user_repos_import).to receive(:call).with(login)
+    worker.perform(login)
   end
 end
