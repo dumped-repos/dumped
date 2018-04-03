bc338704a84ea37332cd328d47e4c4c51b417f15
Gustavo Caso <gustavocaso@gmail.com>
2018-04-03 22:36:16 +0200

    display repos in user show

--- lib/dumped/repositories/user_repository.rb
+++ lib/dumped/repositories/user_repository.rb
@@ -3,6 +3,10 @@ class UserRepository < Hanami::Repository
     has_many :repos
   end
 
+  def with_repos(id)
+    aggregate(:repos).map_to(User).by_pk(id).one
+  end
+
   def by_login(login)
     users.where(login: login).one
   end
